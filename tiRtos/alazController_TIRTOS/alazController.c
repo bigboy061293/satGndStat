cb6d6b520fcc020b031b0db99d3fb14ffc146482
bigboy <bigboy.061293@gmail.com>
2019-03-15 02:40:35 +0700

    add

--- tiRtos/alazController_TIRTOS/alazController.c
+++ tiRtos/alazController_TIRTOS/alazController.c
@@ -37,10 +37,12 @@
 /* XDC Module Headers */
 #include <xdc/std.h>
 #include <xdc/runtime/System.h>
+#include <xdc/runtime/Error.h>
 
 /* BIOS Module Headers */
 #include <ti/sysbios/BIOS.h>
 #include <ti/sysbios/knl/Task.h>
+#include <ti/sysbios/knl/Semaphore.h>
 
 /* Example/Board Header files */
 #include "Board.h"
@@ -52,7 +54,7 @@
 // #include <ti/drivers/I2C.h>
 // #include <ti/drivers/SDSPI.h>
 // #include <ti/drivers/SPI.h>
-// #include <ti/drivers/UART.h>
+ #include <ti/drivers/UART.h>
 // #include <ti/drivers/Watchdog.h>
 // #include <ti/drivers/WiFi.h>
 
@@ -61,12 +63,75 @@
  *  Toggle the Board_LED0. The Task_sleep is determined by arg0 which
  *  is configured for the heartBeat Task instance.
  */
+
+
+
+UART_Handle uart;
+
+Semaphore_Handle SEM_uart_rx; // this binary semaphore handles uart receiving interrupts
+
+void UART00_IRQHandler(UART_Handle handle, void *buffer, size_t num);
+
+/*
+ *  ======== fnTaskUART ========
+ *  Task for this function is created statically. See the project's .cfg file.
+ */
+Void fnTaskUART(UArg arg0, UArg arg1)
+{
+    char input;
+    UART_Params uartParams;
+    Error_Block eb;
+    Semaphore_Params sem_params;
+
+
+    /* Create a UART with data processing off. */
+    UART_Params_init(&uartParams);
+    uartParams.writeDataMode = UART_DATA_BINARY;
+    uartParams.readDataMode = UART_DATA_BINARY;
+    uartParams.readReturnMode = UART_RETURN_FULL;
+    uartParams.readEcho = UART_ECHO_OFF;
+    uartParams.baudRate = 9600;
+    uartParams.readMode = UART_MODE_CALLBACK; // the uart uses a read interrupt
+    uartParams.readCallback = &UART00_IRQHandler; // function called when the uart interrupt fires
+    uart = UART_open(Board_UART0, &uartParams);
+
+
+    if (uart == NULL) {
+        System_abort("Error opening the UART");
+    }
+
+    Semaphore_Params_init(&sem_params);
+    sem_params.mode = Semaphore_Mode_BINARY;
+
+    SEM_uart_rx = Semaphore_create(0, &sem_params, &eb);
+    while (1) {
+        UART_read(uart, &input, 1); // prime the uart bus to read the first character, non blocking
+        Semaphore_pend(SEM_uart_rx, BIOS_WAIT_FOREVER); // when a character is received via UART, the interrupt handler will release the binary semaphore
+        // in my case: I get an interrupt for a single character, no need to loop.
+        GPIO_toggle(Board_GPIO_LED1); // LED B - visual clue that we've received a request over USB
+        UART_write(uart, &input, 1);  // as a test, let's just echo the char
+        GPIO_toggle(Board_GPIO_LED1); // LED B - visual clue off
+    }
+}
+
+/* EUSCI A0 UART ISR - The interrupt handler fr UART0 (USB)
+ * this method releases the TI-RTOS binary semaphore SEM_uart_rx
+ * and uartFxn() will process the received c
+ */
+
+void UART00_IRQHandler(UART_Handle handle, void *buffer, size_t num)
+{
+    Semaphore_post(SEM_uart_rx);
+}
+
+
 Void heartBeatFxn(UArg arg0, UArg arg1)
 {
     while (1) {
         Task_sleep((UInt)arg0);
         GPIO_toggle(Board_GPIO_LED0);
-        System_printf("LED here!!!");
+        //System_printf("LED here!!!");
+
     }
 }
 
@@ -79,6 +144,10 @@ Board_initADC()
 {
     ADC_init();
 }
+Board_initUART()
+{
+    UART_init();
+}
 /*
  *  ======== main ========
  */
@@ -89,6 +158,8 @@ int main()
     Board_initGeneral();
     Board_initGPIO();
     Board_initADC();
+    Board_initUART();
+
 
     GPIO_write(Board_GPIO_LED0, Board_GPIO_LED_ON);
 
