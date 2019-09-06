6e3cfc1df38690d0b2fc92cc4728b6b579129c7d
bigboy <bigboy.061293@gmail.com>
2019-09-06 11:23:05 +0700

    update RF

--- mainBrain/bringItToLifeIO/src/main.cpp
+++ mainBrain/bringItToLifeIO/src/main.cpp
@@ -1,6 +1,6 @@
 #include "includeAllStuff.h"
 #include "pinStateConstants.h"
-uint8_t MODE = 6;
+uint8_t MODE = 7; // default mode
 unsigned long tempElDesired = 0;
 unsigned long tempAzDesired = 0;
 
@@ -272,21 +272,46 @@ switch (MODE)
 
   case MODE_PC_EL:  //  7  Mode verify angle by Smartphone via serial1 //
   {
-    if (motorEl.homingDone == 1)
+    //if (motorEl.homingDone == 1)
+    //motorAz.isControlled = 1;
+    motorEl.isControlled = 1;
     {
       while(1)
       {
-        Serial.setTimeout(1000); // 1000 ms
+        Serial.setTimeout(300000); // 1000 ms
         //waiting for input
         while (Serial.available() == 0);
         float val = Serial.parseFloat(); //read int or parseFloat for ..float...
-        Serial.println(val);
-        motorAz.desiredPulse = val*4444.444;
+        //Serial.println(val);
+        motorEl.desiredPulse = val*4444.444;
+        while (motorEl.desiredPulse != motorEl.currentPulse)
+        {
+
+        Serial.println(motorEl.currentPulse/4444.44);
+        delay(10);
+        }
       }
     }
     break;
   };
 
+  case MODE_PC_AZ:  //  8  Mode verify angle by Smartphone via serial1 //
+  {
+    if (motorAz.homingDone == 1)
+    {
+      while(1)
+      {
+        Serial.setTimeout(300000); // 1000 ms
+        //waiting for input
+        while (Serial.available() == 0);
+        float val = Serial.parseFloat(); //read int or parseFloat for ..float...
+        //Serial.println(val);
+        motorAz.desiredPulse = val*888.89;
+        Serial.println(motorAz.currentPulse/888.89);
+      }
+    }
+    break;
+  };
 
   default:
   {
